61de9a3a9b4eca24a7afb545a373d73eb8ea2313
Haris Sabanovic <hasa2303@student.miun.se>
2024-01-29 13:57:41 +0100

    funktion som filtrerar beroende på input

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const url = "https://dahlgren.miun.se/ramschema_ht23.php";
 let courseCodeEl = document.getElementById("course-code");
 let courseNameEl = document.getElementById("course-name");
 let courseProgressionEl = document.getElementById("course-progression");
+let InputEl = document.getElementById("search");
+
+
 
 window.onload = init;
 
@@ -15,9 +18,11 @@ async function init() {
 
         displayCourses(courses);
 
+
         courseCodeEl.addEventListener("click", sortCourseCode);
         courseNameEl.addEventListener("click", sortCourseName);
         courseProgressionEl.addEventListener("click", sortCourseProgression);
+        InputEl.addEventListener("input", filterCourses);
 
 
     } catch {
@@ -26,6 +31,7 @@ async function init() {
 }
 
 function displayCourses(courses) {
+
     let coursesEl = document.getElementById("course-list");
     coursesEl.innerHTML = "";
 
@@ -73,6 +79,28 @@ async function sortCourseProgression() {
         courses.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
 
         displayCourses(courses); 
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+async function filterCourses() {
+    try {
+        const response = await fetch(url);
+        let courses = await response.json();
+
+        const inputValue = InputEl.value
+        
+        const filteredCourses = courses.filter(course => {
+            // You can customize the filtering logic based on your requirements
+            return (
+                course.code.toLowerCase().includes(inputValue) ||
+                course.coursename.toLowerCase().includes(inputValue) ||
+                course.progression.toLowerCase().includes(inputValue)
+            );
+        });
+
+        displayCourses(filteredCourses);
     } catch (error) {
         console.log("Error:", error);
     }
